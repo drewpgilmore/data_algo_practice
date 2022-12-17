632c74cefdcadd7e3cde78be0523d1139101276a
drewpgilmore <drewpgilmore@gmail.com>
2022-12-16 17:02:20 -0800

    add outline of core functions

--- c/linkedlist.h
+++ c/linkedlist.h
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+// linkedlist.h
+// headers for node struct and linkedlist core functions
 
 typedef struct node {
     int data; 
     struct node *next;
 } node; 
 
-node makeList(int length) {
+// testing functions
+node *makeList(int length) {
     node *llist = NULL;
     for (int i = 0; i < length; i++) {
         node *n = malloc(sizeof(node));
@@ -14,5 +15,66 @@ node makeList(int length) {
         n->next = llist;
         llist = n;
     }
-    return *llist;
+    return llist;
 }
+
+// core functions
+node *llist(int arr[]) {
+    // returns linked list from array of ints
+    
+}
+
+int length(node *llist) {
+    // returns length of list
+    // node *ptr = &llist;
+    // while (ptr != NULL) {
+    //     printf("%i -> ", ptr->data);
+    //     ptr = ptr->next;
+    // }
+    // printf("End\n");
+    return 1
+}
+
+char *print(node *llist) {
+    // prints list
+    return "list string"
+}
+
+node *traverseTo(node *llist, int index) {
+    // returns pointer to node at index
+    return node
+}
+
+node *append(node *llist, node n) {
+    // appends node "n" to list
+    return llist
+}
+
+node *insertBefore(node *llist, node n, node before) {
+    // inserts node "n" before node "before"
+    return llist
+}
+
+bool contains(node *llist, node n) {
+    // returns bool if node "n" is in list
+    return 0
+}
+
+node *deleteNode(node *llist, node n, char *method) {
+    // deletes node "n" from llist according to "method"
+    // method = "any" deletes all instances of node "n"
+    // method = "first" deletes first instance of node "n"
+    return llist
+}
+
+node *reverse(node *llist) {
+    // reverses llist without creating new list
+    return llist
+}
+
+node *sort(node *llist, char *method) {
+    // sorts list according to "method"
+    // method = "ascending" or "descending"
+    return llist
+}
+
