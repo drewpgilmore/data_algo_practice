c289910bcd241873b030bbbb7492ac5afe43360f
drewpgilmore <drewpgilmore@gmail.com>
2022-12-21 12:51:52 -0800

    update test file to handle core functions

--- c/test.c
+++ c/test.c
@@ -1,17 +1,33 @@
-// place to test functionality
+/* Testing of data structures */
 #include <stdio.h>
 #include <stdlib.h>
 #include "linkedlist.h"
 
 int main(void) {
-    // implement list with random ints
-    node llist = makeList(10);
+    node *llist = makeList(10);
     
-    // iterate through list, print values
-    node *ptr = &llist;
-    while (ptr != NULL) {
-        printf("%i -> ", ptr->data);
-        ptr = ptr->next;
-    }
-    printf("End\n");
+    /* Test print */
+    print(llist);
+
+    /* Test length */
+    int l = length(llist);
+    printf("Length is %i\n", l);
+
+    /* Test traverse to */
+    int index = 3;
+    node *targetNode = traverseTo(llist, index);
+    printf("Node at index %i is %i\n", index, targetNode->data);
+
+    /* Test add node */
+    llist = addNode(llist, 3, "append");
+    llist = addNode(llist, 12, "prepend");
+    llist = addNode(llist, 14, "faulty input");
+    print(llist);
+
+    /* Test delete node */
+    node *n = traverseTo(llist, 4); 
+    printf("Deleting node %i\n", n->data);
+    llist = deleteNode(llist, n); 
+    print(llist);
+
 }
