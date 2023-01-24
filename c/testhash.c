b8a207c48525b719e7e80021189ff639f987d780
drewpgilmore <drewpgilmore@gmail.com>
2023-01-23 22:09:08 -0800

    test hash table with random nodes

--- c/testhash.c
+++ c/testhash.c
@@ -3,23 +3,32 @@
 #include <ctype.h>
 #include "hashtable.h"
 
+/* HASH TABLE
+    - Node struct = {.string = char *, .num = int .next = node *}
+      node displays as "(node->string, node->num)"
+    - Nodes are stored in the table by hashing first char in .string
+    - Nodes with same hash form linked list sorted alphabetically by .string
+
+    Samle output of printTable():
+    0: ---
+    1: (Samwise, 40) -> (Shadowfax, 7) -> End  
+    2: (Bilbo, 111) -> (Boromir, 80) -> End
+    ...
+    n: (Gandalf, 100000) -> (Gimli, 90) -> End
+
+*/
+
 int main() {
-    
+    // Start with empty table
     clearTable();
-    node drew      = {.string="Drew",      .num=29};
-    node lauren    = {.string="Lauren",    .num=29};
-    node georgia   = {.string="Georgia",   .num=4};
-    node clark     = {.string="Clark",     .num=2};
-    node charlotte = {.string="Charlotte", .num=0};
+
+    // Insert random nodes
+    for (int i = 0; i < 50; i++) {
+        node *n = malloc(sizeof(node)); 
+        n = randomNode(); 
+        insertPerson(n);
+    }
     
-    insertPerson(&drew);
-    insertPerson(&charlotte);
-    insertPerson(&clark);
-    insertPerson(&lauren);
-    insertPerson(&georgia);
-    printTable();
-
-    node *result = search("Bill"); 
-    //printf("%s: %i", result->string, result->num);
-
+    // Display entire table
+    printList(&hashTable[8], "all");
 }
